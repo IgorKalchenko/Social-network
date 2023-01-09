fb2ba3c087f929d9102add780a7d2eb6ec24fdc9
Igor Kalchenko <igorkalchenko1997@gmail.com>
2023-01-09 12:41:13 +0300

    Update forms.py

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -5,6 +5,10 @@ User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email')
