5b1090bd602f78110851a068343b041e6e46d4aa
Igor Kalchenko <igorkalchenko1997@gmail.com>
2023-01-09 12:45:50 +0300

    Update models.py

--- yatube/core/models.py
+++ yatube/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CreatedModel(models.Model):
-    """Абстрактная модель. Добавляет дату создания."""
+    """Abstract model to add publication date."""
     pub_date = models.DateTimeField(
         verbose_name='Дата публикации',
         auto_now_add=True
