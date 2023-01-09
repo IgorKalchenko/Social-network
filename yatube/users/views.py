5f59841a1b01024cb46f302c2e8eb2ccdc4d9d4e
Igor Kalchenko <igorkalchenko1997@gmail.com>
2023-01-09 12:44:05 +0300

    Update views.py

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -5,6 +5,9 @@ from .forms import CreationForm
 
 
 class SignUp(CreateView):
+    """
+    A View that manages sign up process of a user.
+    """
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
